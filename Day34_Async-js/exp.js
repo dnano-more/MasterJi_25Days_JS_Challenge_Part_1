09fb2d453d752fdc8df11e636f3580f25931be80
Dnyaneshwar More <dnyaneshwarmore2500@gmail.com>
2025-11-28 22:36:41 +0530

    feat: add async JavaScript examples with setInterval and setTimeout

---
+++ Day34_Async-js/exp.js
@@ -0,0 +1,13 @@
+console.log('hello async js');
+
+const add = 2 + 2;
+console.log(add);
+
+add = undefined;
+console.log(add);
+
+
+console.log('last console hun main');
+
+let txt = 'hello'
+console.log(txt);
