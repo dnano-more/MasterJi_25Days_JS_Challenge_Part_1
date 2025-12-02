125855e1dab4cc76fbf983ee216de1e86710c592
Dnyaneshwar More <dnyaneshwarmore2500@gmail.com>
2025-12-02 22:55:26 +0530

    feat: implement various Promise examples with error handling and async/await

---
+++ Day36_Promises/promis.js
@@ -0,0 +1,93 @@
+// Creating a Promise
+const onePromise = new Promise(function (resolve, reject) {
+    // Do an async task
+    // DB calls, cryptography, network
+    setTimeout(function () {
+        console.log("Async task is completed.");
+        resolve();
+    }, 1000);
+});
+
+// Handling Promise
+onePromise.then(function () {
+    console.log("Promise consumed!");
+})
+
+
+// Chained Promises 
+new Promise(function (hoGaya, nahiHuva) {
+    setTimeout(function () {
+        console.log("Async task two");
+        hoGaya();
+    }, 1000);
+}).then(function () {
+    console.log("Async two resolved!");
+});
+
+
+// Promises with data
+const promiseThree = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve({ username: "nano", email: "nano@netflix.com" });
+    }, 1000)
+})
+
+promiseThree.then(function (user) {
+    console.log(user);
+})
+
+
+// Promises with error handling
+const promiseFour = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let error = true;
+        if (!error) {
+            resolve({ username: "dnano", email: "nano@amazon.com" });
+        } else {
+            reject('ERROR: Something went wrong');
+        }
+    }, 1000)
+})
+
+// const username = promiseFour.then((user) => {
+//     console.log(user);
+//     return user.username
+// })
+// console.log(username);
+
+promiseFour
+    .then((user) => {
+        console.log(user);
+        return user.username
+    })
+    .then((myUsername) => {
+        console.log(myUsername);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .finally(() => console.log("The promise is either resolve or rejected."))
+
+
+// Promises with async await
+const promiseFive = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let error = true;
+        if (!error) {
+            resolve({ username: "Sachin", email: "sachin@microsoft.com" });
+        } else {
+            reject('ERROR: microsoft went wrong');
+        }
+    }, 1000)
+})
+
+// Situation like -  DB connection hua hi nahi to aage nahi jana ho to .then() ki bajay async, await laga sakto ho for example⬇
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive;
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumePromiseFive();
